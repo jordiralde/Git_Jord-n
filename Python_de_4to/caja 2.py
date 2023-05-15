cbda19d78471d55f85bf138e55c1462bd259c72a
jordiralde <jiralde@escuelasproa.edu.ar>
2023-05-15 12:11:13 -0300

    Trabajo
    Trabajo de volumen y altura de caja

---
+++ Python_de_4to/caja 2.py
@@ -0,0 +1,89 @@
+print("inicio")
+'''PROGRAMA HECHO POR JORDAN, MateoT
+Calculo:            V= Largo * Ancho * height           (Ancho - (h.x)) * (Largo - (h.x)) = Calcular el maximo volumem
+            ¿y para imprimir altura y volumen de esa caja?
+'''
+#listas en donde se van a ingresar los dato de altura y volumen
+listaV = []
+listaH = []
+listaMax = []
+#se crea la funcion menu para saber si su eleccion es 1 o 2
+def menu():
+    print('''
+    Ingrese 1 para hacer un calculo
+    Ingrese 2 para salir
+    ''')
+def valores():
+    print(f"los valores son: {Altura}")
+    print("altura son: ",listaH)
+    print(f"la lista es: {listaV}")
+    comparate = list(zip(listaV, listaH))
+    print(f"los datos junto a sus respectivas alturas son: {comparate}")
+while True: #Bucle while para que los datos ingresados entren sin error al programa
+    try:
+        menu()#se llama a menu()
+        felection = int(input(f"Ingrese..."))   #opciones del menu
+        while felection > 0 and felection < 3:
+                if felection == 1:
+                        #valores de una caja de carton
+                        h=0 
+                        rango = int(input("Ingrese cantidad de iteraciones: "))
+                        ancho = int(input("El ancho para la plancha de carton: "))
+                        largo = int(input("El largo para la plancha de carton: "))
+                        #Se piden los datos iniciales como el rango ancho y largo
+                        for h in range(rango):
+                            print("calculo ", (h+1))
+                            h += 1
+                            anchoh = ancho - h              #Ancho - h
+                            largoh = largo - h              #Largo - h
+                            volumen = anchoh * largoh * h   #Se calcula el volumen
+                            print(f"para el ancho: {ancho} - {h} es {anchoh}")
+                            print(f"para el largo: {largo} - {h} es {largoh}")
+                            print(f"para el area: {anchoh} * {largoh} * {h} = {volumen}")
+                            #Se imprimen los valores, se almacenan: volumen y h en las listas
+                            listaH.append(h)
+                            listaV.append(volumen)
+                            print("---------------------------")
+                            #Esto restringe el programa a valores negativos
+                            if anchoh and largoh and volumen < 0:
+                                print("El codigo no puede llegar a valores negativos :/")
+                                break   #Termina el ciclo si el valor llega a negativo
+                        #Aca empieza a imprimir el volumen y altura
+                        max_area = max(listaV)  #Se usa max() para el valor más alto
+                        while True:             #Esto hace el calculo del index
+                            for h in range(rango):
+                                Altura = listaV.index(max_area) #Con la funcion index() se puede encontrar la ubicacion de un numero especifico
+                                Altura +=1                      #de una lista, en este caso el maximo es max_area
+                            break
+                        for i in range(3):  #Esto solo separa codigo
+                            print("...")
+                        #El usuario decide si desea llamar a los valores
+                        print("quiere llamar a los valores?, elija 1, o ingrese otro numero para saber el max y h")
+                        valores = int(input("..."))
+                        if valores == 1:    #Si se cumple valor == 1 se van a llamar a los datos    (1)
+                            valores()
+                        else:
+                            print("...")   #Si se ingresa otro numero
+                        for i in range(3):  #Esto solo separa codigo
+                            print("...")
+                        print(f"los datos son: [{max_area}] como el maximo volumen y [{Altura}] como la altura de esa caja")#Se imprime la caja
+                        #list()     
+                        #zip()
+                        #index()
+                        break   #Esto vuelve al primer while para llamar al menu y felection
+                else:
+                    print(f"cerrando...          [{felection}]")
+                    break
+        break
+    except ValueError:  #exepcion de caracter en datos ingresados
+        print("Dato no valiido, reinicio al menu()")
+        print("---------------------------")
+
+'''
+print("ingrese que tan tonto es gordi")
+quetantontoesjordan=(int(input()))
+list=[]
+list.append(quetantontoesjordan)
+print("jordan es de tonto: ", quetantontoesjordan)
+'''
+print("fin")
---
+++ Python_de_4to/def.py
@@ -0,0 +1,26 @@
+'''
+pprint("inicio")
+"""
+def menu(7):
+    inicio=10
+    print(inicio)
+    print("en la funcion menu")
+    return menu  4
+
+menu()
+print(menu)
+print(inicio)
+"""
+def my_function(x):
+  return 4 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+print("fin")
+'''             
+print("Inicio")
+print("fin")
+print("hola, en este programa podemos, sumar, restar,dividir, multiplicar")
+entrada=0
