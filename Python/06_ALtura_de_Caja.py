b91ec9a968b2806d65972251c0fe9eb52c1dd9f9
Jordán Iralde <jiralde@escuelasproa.edu.ar>
2023-06-08 18:31:11 -0300

    last update

--- Python/06_ALtura_de_Caja.py
+++ Python/06_ALtura_de_Caja.py
@@ -1,32 +1,29 @@
-print("inicio")
+print("inicio")     #   (largo - 2 * h) * (ancho - 2 * h)
 listaVolumen = []
-h = 1
+h = 0
+i = 0
 while True:
     try:
         largo = int(input("largo de la caja "))
         ancho = int(input("ancho de la caja "))
-        rango = int(input("rango: "))
-        for i in range(rango):
+
+        largo2h = largo - 2 * h
+        ancho2h = ancho - 2 * h
+        volumen = largo2h * ancho2h
+        while True:
             print(f"calculo numero {i+1} \n")
             h = h+1
             print(f"el valor de h es {h}")
-            
-            
-            largo2 = largo - 2
-            ancho2 = ancho - 2
-            
-            largo2h = largo2 * h
-            ancho2h = ancho2  * h
-
+            largo2h = largo - 2 * h
+            ancho2h = ancho - 2 * h
             volumen = largo2h * ancho2h
             listaVolumen.append(volumen)
-            listaVolumen.sort(reverse=True)
-            print(f"largo2h= {largo} - 2({largo2}) * {h} = {largo2h}")
-            print(f"ancho2h= {ancho} - 2({ancho2}) * {h} = {ancho2h}")
+            print(f"largo2h= {largo} - 2 * {h} = {largo2h}")
+            print(f"ancho2h= {ancho} - 2 * {h} = {ancho2h}")
             print(f"volumen= {largo2h} * {ancho2h} = {volumen}")
             print("_______________________________\n")
             if largo2h and ancho2h < 1:
-                print("$$$valor negativo$$$")
+                print("$$$valores inferiores a 1$$$")
                 break
         print(listaVolumen)
         print(f"El valor maximo es= {max(listaVolumen)}")
