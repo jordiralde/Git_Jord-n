dfc5c9ca3d79491b85882dc055163c9ff7971136
jordiralde <jiralde@escuelasproa.edu.ar>
2023-09-27 10:47:38 -0300

    merges

--- Python/13_comprobador_de_Variables.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Cargar el conjunto de datos desde un archivo CSV
-#df = pd.read_csv("glosario.csv")
-
-url = 'https://drive.google.com/uc?id=1MqW2EpcIAnmffzu64-bq4kBv-07y9ILT'
-df = pd.read_csv(url)
-
-print (df)
