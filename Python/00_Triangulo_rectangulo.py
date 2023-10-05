2949a34ac2af898c9f9049bec74c4eb1ed7f715f
jordiralde <jiralde@escuelasproa.edu.ar>
2023-10-05 09:57:08 -0300

    commit

--- Python/00_Triangulo_rectangulo.py
+++ Python/00_Triangulo_rectangulo.py
@@ -1,12 +1,8 @@
-num = 0
-s='abcde'
-t='abcdefhg'
-def FuncionDiferencias(s: str, t: str, num):
-    for i in t:
-        num+=1
-        print(i, (num))
-        print(t, (num))
-        
-        if s.count(i) != t.count(i):
-            return print(f"letra demas= {i}")
-FuncionDiferencias(s, t, num)
+suma = 0
+
+for numero in range(1, 36):
+    suma += numero
+    print("operacion ", numero)
+    print(suma)
+  
+print(suma)
