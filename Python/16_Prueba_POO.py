58043e8885efd99bfad6e31ca49ef23988dff81e
jordiralde <jiralde@escuelasproa.edu.ar>
2023-11-15 10:28:57 -0300

    Prueba

--- Python/16_Prueba_POO.py
+++ Python/16_Prueba_POO.py
@@ -1,20 +1,23 @@
-class rectangulo():
-    
-    #Esta clase realizara el calculo del area y del perimetro de un rectangulo
-
-    def __init__(self, longitud, ancho) -> None:
+class Rectangulo():
+    #Perimetro es la suma de todos los lados
+    def __init__(self, longitud, ancho):
         self.longitud = longitud
         self.ancho = ancho
 
-    def calculo_area(self):
-        self.area = self.longitud * self.ancho
-        return print(f"El area del rectangulo es: {self.area}")
-
-    def calculo_perimetro(self):
-        self.perimetro = (self.longitud * 2) + (self.ancho * 2)
-        return print(f"El perimetro del rectangulo es {self.perimetro}")
+    def calcular_area(self):
+            #self.area = self.ancho * self.longitud
+            #return print("El area es: ",self.area)
+        area = self.ancho * self.longitud
+        #return print("El area es: ",area)
+        return area
     
-MiRectangulo = rectangulo(3, 4)
+    def calcular_perimetro(self):
+        perimetro = (self.ancho + self.longitud) * 2
+        return print("El perimetro es: ", perimetro)
+    
+MiRectangulo = Rectangulo(4,5)
 
-MiRectangulo.calculo_area() 
-MiRectangulo.calculo_perimetro()
+    #print(MiRectangulo.calcular_area())
+print("El area es: ", MiRectangulo.calcular_area())
+MiRectangulo.calcular_area()
+MiRectangulo.calcular_perimetro()
