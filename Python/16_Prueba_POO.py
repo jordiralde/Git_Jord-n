f999a3d3c3c33ee535fc1c92b6f7d2476359baff
jordiralde <jiralde@escuelasproa.edu.ar>
2023-11-15 10:58:46 -0300

    Update 16_Prueba_POO.py

--- Python/16_Prueba_POO.py
+++ Python/16_Prueba_POO.py
@@ -5,19 +5,14 @@ class Rectangulo():
         self.ancho = ancho
 
     def calcular_area(self):
-            #self.area = self.ancho * self.longitud
-            #return print("El area es: ",self.area)
         area = self.ancho * self.longitud
-        #return print("El area es: ",area)
         return area
     
     def calcular_perimetro(self):
         perimetro = (self.ancho + self.longitud) * 2
-        return print("El perimetro es: ", perimetro)
+        return perimetro
     
 MiRectangulo = Rectangulo(4,5)
 
-    #print(MiRectangulo.calcular_area())
 print("El area es: ", MiRectangulo.calcular_area())
-MiRectangulo.calcular_area()
-MiRectangulo.calcular_perimetro()
+print("El perimetro es: ", MiRectangulo.calcular_perimetro())
