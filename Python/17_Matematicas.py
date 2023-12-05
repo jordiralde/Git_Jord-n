119aae426ee28db2c335c2fdb9ea194fff199644
jordiralde <jiralde@escuelasproa.edu.ar>
2023-12-05 12:11:37 -0300

    mates

--- Python/17_Matematicas.py
+++ Python/17_Matematicas.py
@@ -15,14 +15,18 @@ def MenuCalculos():
     return print(Menu1)
 
 def Calculo_Perimetro():
-    CantidadLados = int(input("Ingrese el numero de lados que contiene: "))
-    lados = int(input("Ingrese los cm los lados contienen: "))
-    while lados < 3:
+    while True :
         try:
-            lados = int(input("Ingrese el numero de lados que contiene: "))
+            CantidadLados = int(input("Ingrese el numero de lados que contiene: "))
+            if CantidadLados < 3:
+                print("No se puede")
+                break
+            CmDeLados = int(input("Ingrese los cm los lados contienen: "))
+            perimetro = CmDeLados * CantidadLados   #De un poligono regular
+
+            break
         except ValueError():
             print("Error")
-    perimetro = lados * x   #De un poligono regular
     return print("El perimetro es: ", perimetro)
 
 def Calculo_Area(a, b):
@@ -38,13 +42,19 @@ def Multi_Divis_Monomios():
     print("Cuanto es 4a^2 * 2a^2?")
 
 while True:
+    MenuCalculos()
     try:
-        MenuCalculos()
         Eleccion = int(input("Ingrese su eleccion: "))
         if Eleccion == 1:
-            Calculo_Perimetro()
+            #Calculo_Figuras()
+            print()
 
         elif Eleccion == 2:
-            Calculo_Area()
+            #Calculo_NumeroOperaciones()
+            print()
+            
+        elif Eleccion == 3:
+            Calculo_Perimetro()
+
     except ValueError():
         print("Error, dato no valido")
